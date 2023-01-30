245805120006b6d4ed51724f7c09ed632a42a642
s22039 <80396358+s22039@users.noreply.github.com>
2023-01-30 07:20:26 +0100

    Add files via upload

---
+++ image.js
@@ -0,0 +1,12 @@
+	function changeImg1() {
+    document.getElementById('myImg').src="images/1.jpg"
+     }
+                        
+	function changeImg2() {
+    document.getElementById('myImg').src="images/2.jpg"
+     }
+						
+	function changeImg3() {
+    document.getElementById('myImg').src="images/3.jpg"
+    }
+						
